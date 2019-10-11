3251164b565cdd3941e959e18b1d71d931f71a72
Antoine CELLIER <antoine.cellier@zenika.com>
2019-10-11 15:55:54 +0200

    :lipstick: improve navbar UI

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { func, object } from 'prop-types'
 import { Layout, Menu, Button } from 'antd'
+import './navbar.css'
 
 const { Header } = Layout
 
@@ -11,20 +12,21 @@ const Navbar = ({
 }) => (
   <Header>
     <div className="logo" />
-    <Menu
-      theme="dark"
-      mode="horizontal"
-      defaultSelectedKeys={['2']}
-      style={{ lineHeight: '64px' }}
-    >
-      <Menu.Item key="1">nav 1</Menu.Item>
-      <Menu.Item key="2">nav 2</Menu.Item>
-      <Menu.Item key="3">
+    <div className="navbar">
+      <div className="items">
+        <div className="item">
+          Menu 1
+        </div>
+        <div className="item">
+          Menu 2
+        </div>
+      </div>
+      <div className="extra">
         <Button type="primary" onClick={user ? onLogout : onLogin}>
           {user ? 'Logout' : 'Login'}
         </Button>
-      </Menu.Item>
-    </Menu>
+      </div>
+    </div>
   </Header>
 )
 
